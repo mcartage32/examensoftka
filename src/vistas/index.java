1b1ad27cef63315441ec7b883d3f720d1d98bfef
mcartage32 <mcartage33@gmail.com>
2021-09-11 00:00:54 -0500

    Union entre vistas y controlador

--- src/vistas/index.java
+++ src/vistas/index.java
@@ -1,8 +1,11 @@
 package vistas;
 
+import controlador.Controlador;
+
 
 public class index extends javax.swing.JFrame {
-
+   
+      private static ingresar_preguntas objin = new ingresar_preguntas();
 
     public index() {
         initComponents();
@@ -10,7 +13,14 @@ public class index extends javax.swing.JFrame {
         setTitle("Concurso Preguntas y Respuestas");
     }
 
-
+    
+     public static void main(String args[]) {
+    
+        new index().setVisible(true);
+        Controlador ctrl = new Controlador(objin);
+       
+    }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -48,47 +58,11 @@ public class index extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAgregarPreguntasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarPreguntasActionPerformed
-        ingresar_preguntas objin = new ingresar_preguntas();
         objin.setVisible(true);
         objin.setLocationRelativeTo(null);
         objin.setTitle("Ingresar Preguntas");
         dispose();
     }//GEN-LAST:event_btnAgregarPreguntasActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(index.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(index.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(index.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(index.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new index().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JButton btnAgregarPreguntas;
